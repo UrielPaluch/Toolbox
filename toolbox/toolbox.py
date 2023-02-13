8d40a7b7fffc73c259bb89d235dd6beadcac511f
UrielPaluch <uripaluch.2@gmail.com>
2023-02-12 21:14:00 -0300

    init commit

---
+++ toolbox/toolbox.py
@@ -0,0 +1,11 @@
+
+def get_feriados_byma() -> list:
+    """
+    Devuelve una lista de strings con fechas de todos los feriados de byma
+    """
+    feriados_byma = ['2023-02-20', '2023-02-21', '2023-03-24', '2023-04-06',
+                     '2023-04-07', '2023-05-01', '2023-05-25', '2023-05-26',
+                     '2023-06-19', '2023-06-20', '2023-08-21', '2023-10-13',
+                     '2023-10-16', '2023-11-20', '2023-12-08', '2023-12-25']
+
+    return feriados_byma
