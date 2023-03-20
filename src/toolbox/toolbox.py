985b13957286be9bd4a6d372b43cf7ea3e51beaf
UrielPaluch <uripaluch.2@gmail.com>
2023-03-19 21:14:41 -0300

    feat: add new function calculo_plazo_liquidacion()

--- src/toolbox/toolbox.py
+++ src/toolbox/toolbox.py
@@ -1,6 +1,8 @@
 """
     Define las funciones de la libreria
 """
+import datetime
+import numpy as np
 
 def get_feriados_byma() -> list['str']:
     """
@@ -17,3 +19,35 @@ def get_feriados_byma() -> list['str']:
                      '2023-10-16', '2023-11-20', '2023-12-08', '2023-12-25']
 
     return feriados_byma
+
+def calculo_plazo_liquidacion() -> int:
+    """ Plazo de liquidacion en dias para el calculo de la tasa
+
+    Returns
+    ----------
+    int
+        Proximo plazo de liquidacion en dias
+
+    """
+    # Los feriados tienen que estar en orden con el formato yyyy-mm-dd
+    # Se extraen los feriados de https://www.byma.com.ar/servicios/calendario-bursatil/
+    # para cargarlos en la variable feriados_byma
+    feriados_byma = ['2023-02-20', '2023-02-21', '2023-03-24', '2023-04-06',
+                     '2023-04-07', '2023-05-01', '2023-05-25', '2023-05-26',
+                     '2023-06-19', '2023-06-20', '2023-08-21', '2023-10-13',
+                     '2023-10-16', '2023-11-20', '2023-12-08', '2023-12-25']
+    hoy = datetime.date.today()
+    dia_liquidacion = hoy
+
+    count_dias_habiles = 0
+
+    while count_dias_habiles < 2:
+        dia_liquidacion = dia_liquidacion + \
+                        datetime.timedelta(days = 1)
+
+        if np.is_busday(dia_liquidacion, holidays = feriados_byma):
+            count_dias_habiles += 1
+
+    diff_days = dia_liquidacion - hoy
+
+    return diff_days.days
