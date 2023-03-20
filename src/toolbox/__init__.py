1433af66dd5eb6077d2e0231b0e3a72080ba34b0
UrielPaluch <uripaluch.2@gmail.com>
2023-03-20 00:35:03 -0300

    fix: expongo la funcion del logging_module.my_logger en el src/__init__

--- src/toolbox/__init__.py
+++ src/toolbox/__init__.py
@@ -6,3 +6,5 @@ from .toolbox import get_feriados_byma
 from .toolbox import calculo_plazo_liquidacion
 from .toolbox import hay_mercado
 from .toolbox import extract_price_size_values
+
+from .logging_module import my_logger
