eba2aa238236dc9a196b6f4f880e7f31684208d0
UrielPaluch <uripaluch.2@gmail.com>
2023-03-19 21:11:28 -0300

    style: update docstring from get_feriados_byma()

--- src/toolbox/toolbox.py
+++ src/toolbox/toolbox.py
@@ -5,6 +5,11 @@
 def get_feriados_byma() -> list['str']:
     """
     Devuelve una lista de strings con fechas de todos los feriados de byma
+
+    Returns
+    ----------
+    list[str]
+        Lista de str con los dias feriados
     """
     feriados_byma = ['2023-02-20', '2023-02-21', '2023-03-24', '2023-04-06',
                      '2023-04-07', '2023-05-01', '2023-05-25', '2023-05-26',
