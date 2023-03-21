b695d5b2550fe30d77559562a4eade2825b57ed9
UrielPaluch <uripaluch.2@gmail.com>
2023-03-20 21:57:00 -0300

    test: fix in importing modules

--- tests/test_toolbox.py
+++ tests/test_toolbox.py
@@ -1,11 +1,11 @@
 # pylint: disable=missing-docstring
 import datetime
 
-from toolbox import get_feriados_byma
-from toolbox import calculo_plazo_liquidacion_48hs
-from toolbox import calculo_plazo_liquidacion_24hs
-from toolbox import hay_mercado
-from toolbox import extract_price_size_values
+from toolbox.toolbox import get_feriados_byma
+from toolbox.toolbox import calculo_plazo_liquidacion_48hs
+from toolbox.toolbox import calculo_plazo_liquidacion_24hs
+from toolbox.toolbox import hay_mercado
+from toolbox.toolbox import extract_price_size_values
 
 
 def test_get_feriados_byma():
