04d530f5836123a4a7543504099b6dbc178c10c6
Eitan-Sprejer <eitusprejer@gmail.com>
2023-08-29 22:33:46 -0300

    More pylint error fixing

--- src/toolbox/logging_module/logging_module_test.py
+++ src/toolbox/logging_module/logging_module_test.py
@@ -6,6 +6,10 @@ import yaml
 import time
 
 class MyBatchLogger():
+    """
+    Logger que guarda los mensajes en memoria hasta que se llena la capacidad, \
+    y luego los escribe en el archivo de log.
+    """
     def __init__(self, bot_name: str, capacity: int, testing : bool = True) -> None:
         """Inicializa el logger
 
@@ -45,13 +49,22 @@ class MyBatchLogger():
             path = f"./logs/{bot_name}_{datetime.today().strftime('%Y-%m-%d')}.log"
         return path
     def log(self, message, level=logging.INFO):
+        """
+        Loggea el mensaje en memoria
+        """
         self.logger.log(level, message)
     def flush(self):
+        """
+        Escribe los mensajes en memoria en el archivo de log
+        """
         for handler in self.logger.handlers:
             handler.flush()
             handler.close()
 
 class MyNormalLogger():
+    """
+    Logger que escribe los mensajes en el archivo de log a medida que se van recibiendo.
+    """
     def __init__(self, bot_name: str, testing : bool = True) -> None:
         """Inicializa el logger
 
@@ -72,7 +85,9 @@ class MyNormalLogger():
         self.logger.addHandler(file_handler)
         self.logger.setLevel(logging.DEBUG)
     def get_path_to_log_file(self, bot_name: str) -> str:
-        """Devuelve el path al archivo de logging"""
+        """
+        Devuelve el path al archivo de logging
+        """
 
         # Check if the logs folder exists, and if not, create it
         if not os.path.exists('./logs'):
@@ -85,43 +100,47 @@ class MyNormalLogger():
             path = f"./logs/{bot_name}_{datetime.today().strftime('%Y-%m-%d')}.log"
         return path
     def log(self, message, level=logging.INFO):
+        """
+        Loggea el mensaje en el archivo de log
+        """
         self.logger.log(level, message)
 
-def test_time_difference(n: int, cap: int, message_length: int = 100):
+def test_time_difference(n_logs: int, cap: int, message_length: int = 100):
     """
-    Test the time difference between the batch logger and the normal logger.
+    Testea la diferencia en velocidad entre el BatchLogger y el NormalLogger.
     
-    Parameters
+    Parámetros
     ----------
-    n : int
-        Number of messages to log.
+    n_logs: int
+        Número de mensajes para loggear.
+    def test_time_difference(n_logs: int, cap: int, message_length: int = 100):
     cap: int
-        Capacity of the batch logger.
+        Capacidad del BatchLogger.
     message_length: int
-        Length of the message to log.
+        Longitud del mensaje a Loggear.
     """
     tic = time.time()
     logger = MyBatchLogger('batch', capacity=cap)
-    for i in range(n):
+    for i in range(n_logs):
         logger.log('L'*message_length, level=logging.DEBUG)
     toc1 = time.time()
     logger.flush()
     toc2 = time.time()
-    batch_log_time = toc2 - tic
-    batch_flush_time = toc2 - toc1
+    batch_log_time_diff = toc2 - tic
+    batch_flush_time_diff = toc2 - toc1
 
     tic = time.time()
     logger = MyNormalLogger('normal')
-    for i in range(n):
+    for i in range(n_logs):
         logger.log('L'*message_length, level=logging.DEBUG)
     toc = time.time()
-    normal_logger_time_difference = toc - tic
+    normal_logger_time_diff = toc - tic
 
-    return (batch_log_time, batch_flush_time), normal_logger_time_difference
+    return (batch_log_time_diff, batch_flush_time_diff), normal_logger_time_diff
 
 if __name__ == '__main__':
     # Testing speed difference between both loggers.
-    (batch_log_time, batch_flush_time), normal_time = test_time_difference(n=400000, cap=5000)
+    (batch_log_time, batch_flush_time), normal_time = test_time_difference(n_logs=400000, cap=5000)
     batch_time = batch_log_time + batch_flush_time
 
     print(f'Batch Logger: log time {batch_log_time*1000:.0f}ms, flush time {batch_flush_time*1000:.0f}ms')
