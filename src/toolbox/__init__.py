7150e4a9fab99fb53d9eb0b29a801e0686e2af25
UrielPaluch <uripaluch.2@gmail.com>
2023-11-06 17:45:53 -0300

    Creo funcion calculo_plazo_liquidacion

--- src/toolbox/__init__.py
+++ src/toolbox/__init__.py
@@ -3,8 +3,7 @@
 """
 
 from .toolbox import get_feriados_byma
-from .toolbox import calculo_plazo_liquidacion_24hs
-from .toolbox import calculo_plazo_liquidacion_48hs
+from .toolbox import calculo_plazo_liquidacion
 from .toolbox import hay_mercado
 from .toolbox import extract_price_size_values
 from .toolbox import extract_ticker_market_values
--- tests/test_toolbox.py
+++ tests/test_toolbox.py
@@ -19,68 +19,6 @@ def test_get_feriados_byma():
 
 test_get_feriados_byma()
 
-def test_calculo_plazo_liquidacion_48hs():
-    lunes = '2023-03-20'
-    lunes_expected_return = 2
-    lunes_actual_return = calculo_plazo_liquidacion_48hs(datetime.datetime.strptime(lunes,
-                                                                               "%Y-%m-%d").date())
-
-    assert lunes_expected_return == lunes_actual_return, "Fallo el lunes"
-
-    jueves = '2023-03-30'
-    jueves_expected_return = 4
-    jueves_actual_return = calculo_plazo_liquidacion_48hs(datetime.datetime.strptime(jueves,
-                                                                                "%Y-%m-%d").date())
-
-    assert jueves_expected_return == jueves_actual_return, "Fallo el jueves"
-
-    viernes = '2023-03-31'
-    viernes_expected_return = 4
-    viernes_actual_return = calculo_plazo_liquidacion_48hs(datetime.datetime.strptime(viernes,
-                                                                                 "%Y-%m-%d").date())
-
-    assert viernes_expected_return == viernes_actual_return, "Fallo el viernes"
-
-    feriado = '2023-03-23'
-    feriado_expected_return = 5
-    feriado_actual_return = calculo_plazo_liquidacion_48hs(datetime.datetime.strptime(feriado,
-                                                                                 "%Y-%m-%d").date())
-
-    assert feriado_expected_return == feriado_actual_return, "Fallo el feriado"
-
-test_calculo_plazo_liquidacion_48hs()
-
-def test_calculo_plazo_liquidacion_24hs():
-    lunes = '2023-03-20'
-    lunes_expected_return = 1
-    lunes_actual_return = calculo_plazo_liquidacion_24hs(datetime.datetime.strptime(lunes,
-                                                                               "%Y-%m-%d").date())
-
-    assert lunes_expected_return == lunes_actual_return, "Fallo el lunes"
-
-    jueves = '2023-03-30'
-    jueves_expected_return = 1
-    jueves_actual_return = calculo_plazo_liquidacion_24hs(datetime.datetime.strptime(jueves,
-                                                                                "%Y-%m-%d").date())
-
-    assert jueves_expected_return == jueves_actual_return, "Fallo el jueves"
-
-    viernes = '2023-03-31'
-    viernes_expected_return = 3
-    viernes_actual_return = calculo_plazo_liquidacion_24hs(datetime.datetime.strptime(viernes,
-                                                                                 "%Y-%m-%d").date())
-
-    assert viernes_expected_return == viernes_actual_return, "Fallo el viernes"
-
-    feriado = '2023-03-23'
-    feriado_expected_return = 4
-    feriado_actual_return = calculo_plazo_liquidacion_24hs(datetime.datetime.strptime(feriado,
-                                                                                 "%Y-%m-%d").date())
-
-    assert feriado_expected_return == feriado_actual_return, "Fallo el feriado"
-
-test_calculo_plazo_liquidacion_24hs()
-
 def test_hay_mercado():
     lunes = '2023-03-20'
     lunes_expected_return = True
